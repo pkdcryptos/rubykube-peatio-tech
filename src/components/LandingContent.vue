3c57450e37519697f735ef2dadcb42963ac886c4
SergeySaf <Serega_22@mail.ru>
2017-10-19 20:18:23 +0300

    add chatlio to peatio (#17)

--- src/components/LandingContent.vue
+++ src/components/LandingContent.vue
@@ -494,6 +494,20 @@
 
 <script>
 export default {
-  name: 'landing-content'
+  name: 'landing-content',
+  /*eslint-disable*/
+  mounted: function () {
+    window._chatlio = window._chatlio||[];
+    !function(){
+      var t=document.getElementById("chatlio-widget-embed");
+      if(t&&window.ChatlioReact&&_chatlio.init) return void _chatlio.init(t,ChatlioReact);
+      for(var e=function(t){ return function(){_chatlio.push([t].concat(arguments)) }},i=["configure","identify","track","show","hide","isShown","isOnline", "page"],a=0;a<i.length;a++)
+        _chatlio[i[a]]||(_chatlio[i[a]]=e(i[a]));
+      var n=document.createElement("script"),c=document.getElementsByTagName("script")[0];
+      n.id="chatlio-widget-embed", n.src="https://w.chatlio.com/w.chatlio-widget.js", n.async=!0,n.setAttribute("data-embed-version","2.3");
+       n.setAttribute('data-widget-id','b3d49755-40f4-4079-5992-a5bf40d5defb');
+       c.parentNode.insertBefore(n,c);
+    }();
+  }
 }
 </script>
