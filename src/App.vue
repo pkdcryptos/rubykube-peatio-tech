f3145c49850c513bf10525434e47ed11210594e4
SergeySaf <Serega_22@mail.ru>
2017-10-25 16:33:12 +0300

    chatlio widget to not display until the page is scrolled (#35)

--- src/App.vue
+++ src/App.vue
@@ -15,20 +15,34 @@ export default {
     AppFooter: Footer
   },
   name: 'app',
-
   /*eslint-disable*/
-  mounted: function () {
-    window._chatlio = window._chatlio||[];
-    !function(){
-      var t=document.getElementById("chatlio-widget-embed");
-      if(t&&window.ChatlioReact&&_chatlio.init) return void _chatlio.init(t,ChatlioReact);
-      for(var e=function(t){ return function(){_chatlio.push([t].concat(arguments)) }},i=["configure","identify","track","show","hide","isShown","isOnline", "page"],a=0;a<i.length;a++)
-        _chatlio[i[a]]||(_chatlio[i[a]]=e(i[a]));
-      var n=document.createElement("script"),c=document.getElementsByTagName("script")[0];
-      n.id="chatlio-widget-embed", n.src="https://w.chatlio.com/w.chatlio-widget.js", n.async=!0,n.setAttribute("data-embed-version","2.3");
-       n.setAttribute('data-widget-id','b3d49755-40f4-4079-5992-a5bf40d5defb');
-       c.parentNode.insertBefore(n,c);
-    }();
-  }
-}
-</script>
+ methods: {
+   handleScroll () {
+     this.scrolled = window.scrollY > 0;
+     _chatlio.show();
+   }
+ },
+ created () {
+   window.addEventListener('scroll', this.handleScroll);
+ },
+ destroyed () {
+   window.removeEventListener('scroll', this.handleScroll);
+ },
+
+   /*eslint-disable*/
+   mounted: function () {
+     window._chatlio = window._chatlio||[];
+     !function(){
+       var t=document.getElementById("chatlio-widget-embed");
+       if(t&&window.ChatlioReact&&_chatlio.init) return void _chatlio.init(t,ChatlioReact);
+       for(var e=function(t){ return function(){_chatlio.push([t].concat(arguments)) }},i=["configure","identify","track","show","hide","isShown","isOnline", "page"],a=0;a<i.length;a++)
+         _chatlio[i[a]]||(_chatlio[i[a]]=e(i[a]));
+       var n=document.createElement("script"),c=document.getElementsByTagName("script")[0];
+       n.id="chatlio-widget-embed", n.src="https://w.chatlio.com/w.chatlio-widget.js", n.async=!0,n.setAttribute("data-embed-version","2.3");
+        n.setAttribute('data-widget-id','b3d49755-40f4-4079-5992-a5bf40d5defb');
+        c.parentNode.insertBefore(n,c);
+        _chatlio.hide();
+     }();
+   },
+ }
+ </script>
