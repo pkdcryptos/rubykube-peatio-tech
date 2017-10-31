ca2a8aa2a106c61b1815d7ba8a49c361fad61c7b
emasiuk <toysoldier1927@gmail.com>
2017-10-31 20:20:32 +0300

    improve error message (#45)

--- src/components/Email.vue
+++ src/components/Email.vue
@@ -1,34 +1,61 @@
 <template>
-  <form id="signup" class="formee clearfix" action="#" novalidate="novalidate">
-￼     <div style="float: left"><input v-model="email" name="email" id="email" type="text" style="display: block; width: 100%"></div>
-￼     <div style="float: left"><input v-on:click.prevent="postPost()" class="right inputnew" type="submit" title="Send" value="Submit" style="display: block; width: 100%"></div>
-  </form>
+  <div class="cta-sub no-color">
+    <div class="container">
+      <div class="cta-inner">
+        <div v-if="show">
+          <h1>Start your Peatio Cloud</h1>
+          <p>Please submit your email to receive your peatio.tech demo link.</p>
+          <div class="form">
+            <form id="signup" class="formee clearfix" action="#" novalidate="novalidate">
+        ￼      <div style="float: left">
+                <input v-model="email" name="email" id="email" type="text" style="display: block; width: 100%">
+              </div>
+        ￼      <div style="float: left"><input v-on:click.prevent="postPost()" class="right inputnew" type="submit" title="Send" value="Submit" style="display: block; width: 100%"></div>
+              <div v-if="incorrect > 0" class="alert alert-danger error-message" role="alert">
+                Incorrect email address
+              </div>
+            </form>
+          </div>
+        </div>
+        <div v-else>
+          <h1>
+            <span class="big-text">
+              Thank you!
+            </span><br/>
+            <span class="small-text">
+              We will contact you ASAP!
+            </span>
+          </h1>
+        </div>
+        <div id="response"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
   import axios from 'axios'
-  import swal from 'sweetalert'
 
   export default {
     data () {
       return {
         email: '',
+        show: true,
+        incorrect: 0,
         errors: []
       }
     },
-
     // Pushes posts to the server when called.
     methods: {
       postPost () {
         axios.post(`/subscribers`, {
           email: this.email
         })
-        // swal('good')
         .then(response => {
-          swal('Success')
+          this.show = false
         })
         .catch(e => {
-          swal('Incorrect email')
+          this.incorrect += 1
           this.errors.push(e)
         })
       }
