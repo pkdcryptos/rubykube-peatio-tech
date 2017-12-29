30b9aa9c072f282387679b0327b9cb9f07f55b30
peatio.tech <dbryan@peatio.tech>
2017-12-29 17:28:08 +0200

    Update Header.vue (#59)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,10 +6,10 @@
     <div class="col-md-12">
       <div class="hero-content text-center">
         <h1 class="wow fadeInDown" data-wow-delay="0.1s" style="animation-delay: 0.1s; animation-name: fadeInDown;">Peatio [&#35988;&#35973;] Open-Source Exchange</h1>
-        <h2 class="wow fadeInDown" data-wow-delay="0.5s" style="animation-delay: 0.5s; animation-name: fadeInDown;">Cloud deployment made simple and secure</h2>
+        <h2 class="wow fadeInDown" data-wow-delay="0.5s" style="animation-delay: 0.5s; animation-name: fadeInDown;">Cryptocurrency Exchange Development & Cloud Deployment</h2>
         <p class="wow fadeInDown" data-wow-delay="1.0s" style="animation-delay: 1.0s; animation-name: fadeInDown;">
-          Peatio - Cryptocurrency exchange for managing digital currency.
-          <br class="hidden-xs">We provide Peatio installation and configuration services
+          Peatio - software for managing digital exchange for crypto and fiat currencies
+          <br class="hidden-xs">Peatio.tech - Peatio configuration and installation professionals
         </p>
         <a class="btn btn-primary btn-action page-scroll" data-wow-delay="0.2s" href="#platform" style="animation-delay: 0.2s; animation-name: fadeInDown;">Read more</a>
       </div>
